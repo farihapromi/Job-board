a24b5c4be68b49f6195d464506d1cce667309b3c
Fariha Promi <farihapromi2611@gmail.com>
2025-04-11 22:22:21 +0600

    Add middleware

--- src/app/components/JobRow.tsx
+++ src/app/components/JobRow.tsx
@@ -1,29 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const JobRow = () => {
   return (
-   <>
-    <div className="bg-white p-4 rounded-lg shadow-md flex gap-2">
-       <div className='content-center'>
-        <img 
-        className='size-12'
-        src='https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green-300x300.png'/>
-
-       </div>
-       <div className='grow'>
-        <div className='text-gray-500 text-sm'>Spotify</div>
-        <div className='font-bold mb-2 text-lg'>Product designer</div>
-        <div className='content-end text-gray-600 text-xs'>
-            Remote &middot; New York,Us &middot; Fulltime</div>
-       </div>
-       <div className='content-end text-gray-600 text-sm'>
-         2 weeks ago
-
-       </div>
-
+    <div className="bg-white p-4 rounded-lg shadow-md relative">
+      <div className="absolute top-2 right-4  hover:text-red-500 cursor-pointer">
+        <FontAwesomeIcon className='size-6 text-gray-400' icon={faHeart} />
       </div>
-   </>
-  )
-}
 
-export default JobRow
+      <div className="flex gap-4">
+        <div className="content-center">
+          <img
+            className="size-12"
+            src="https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green-300x300.png"
+            alt="Spotify"
+          />
+        </div>
+
+        <div className="grow">
+          <div className="text-gray-500 text-sm">Spotify</div>
+          <div className="font-bold mb-2 text-lg">Product Designer</div>
+          <div className="text-gray-600 text-xs">
+            Remote &middot; New York, US &middot; Full-time
+          </div>
+        </div>
+
+        <div className="text-gray-600 text-sm content-end">2 weeks ago</div>
+      </div>
+    </div>
+  );
+};
+
+export default JobRow;
---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { authkitMiddleware } from '@workos-inc/authkit-nextjs';
+
+export default authkitMiddleware({
+  middlewareAuth: {
+    enabled: true,
+    unauthenticatedPaths: ['/'],
+  },
+  redirectUri: process.env.WORKOS_REDIRECT_URI,
+});
+
+export const config = {
+  matcher: ['/', '/account/:path*'],
+};
