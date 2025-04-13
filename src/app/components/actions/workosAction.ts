30aa59d02061f391f323f870a39b4b6fd0dfd36f
Fariha Promi <farihapromi2611@gmail.com>
2025-04-13 12:17:11 +0600

    Add form for create company

---
+++ src/app/components/actions/workosAction.ts
@@ -0,0 +1,8 @@
+'use server'
+import { WorkOS } from "@workos-inc/node"
+const workos=new WorkOS(process.env.WORKOS_API_KEY)
+
+
+export async function createCompany(data:FormData){
+    await workos.organizations.createOrganization({name:data.get('newCompany') as string})
+}
--- src/app/new-listing/page.tsx
+++ src/app/new-listing/page.tsx
@@ -1,28 +1,43 @@
 
 import {  withAuth } from "@workos-inc/authkit-nextjs";
-import { WorkOS } from "@workos-inc/node";
+import {  WorkOS } from "@workos-inc/node";
+import { createCompany } from "../components/actions/workosAction";
 export default async function NewListingPage(){
     const {user}=await withAuth();
-    const workos=new WorkOS(process.env.WORKOS_API_KEY);
-    let organizationMemberships=[];
-    if(user){
-      organizationMemberships= await workos.userManagement.listOrganizationMemberships({
-            userId:user.id
-        })
-
-    }
-   
-    return(
-        <>
-        <div className="container">
+    if(!user){
+        return (
+            <div className="container">
             {
                 !user && (
                     <div>you need to be logged in to post a job </div>
                 )
             }
-            {
-                user &&
+
+        </div>
+            
+        )
+       
+    }
+    const workos=new WorkOS(process.env.WORKOS_API_KEY);
+    
+   
+     const organizationMemberships= await workos.userManagement.listOrganizationMemberships({
+            userId:user.id,
+        })
+
+    
+   
+    return(
+        <>
+        <div className="container">
+            
+          
                 <div>
+                    <pre>
+                    {JSON.stringify(organizationMemberships ,null,2)}
+
+                    </pre>
+                   
                      <h1 className="text-lg font-bold mt-6">Your Companies</h1>
                      <p className="text-gray-400 text-sm mb-2">Select a company to create job add for</p>
                      <div className="border border-blue-300 bg-blue-50 p-4 rounded-md">
@@ -31,18 +46,18 @@ export default async function NewListingPage(){
                      </div>
                       <h1 className="text-lg font-bold mt-6">Create a new company</h1>
                       <p className="text-gray-400 text-sm mb-2">To create a job lisitng yopu first need to registred</p>
-                    <form action="" className="flex gap-2">
+                    <form action={ createCompany} className="flex gap-2">
                       
-                        <input type="text" name="" id=""
+                        <input type="text" name="newCompany" id=""
                         className="p-2 border border-gray-400 rounded-md"
                          placeholder="company name" />
-                        <button className="flex gap-2 items-center bg-gray-200 px-4 py-4 rounded-md">
+                        <button  type='submit'className="flex gap-2 items-center bg-gray-200 px-4 py-4 rounded-md">
                             create company 
                             
                         </button>
                     </form>
                 </div>
-            }
+            
             
         </div>
         </>
